1696763ccd9d6f79dcf20de779d859dd19cb5a7d
KarinaBertosh <cherrycake246@gmail.com>
2023-03-20 00:18:36 +0300

    feat: save value in localStorage

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './style.scss';
 
-export class Search extends React.Component {
+export class Search extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      valueChange: JSON.parse(localStorage.getItem("value")!) || '',
+    };
+    
+  }
+
+  saveChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ valueChange: e.target.value });
+    localStorage.setItem("value", JSON.stringify(e.target.value));
+  };
+
   render() {
     return (
       <div className='search'>
-        <input type="search" className="search__input" />
+        <input type="search" className="search__input" value={this.state.valueChange} onChange={this.saveChange}/>
         <button className="button">Search</button>
       </div>
     );
