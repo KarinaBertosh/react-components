ad8e6c6ef84c9587dcc658e101602d9d7ac71633
KarinaBertosh <cherrycake246@gmail.com>
2023-04-13 17:53:54 +0300

    feat: add tests for cardSlice

--- react-redux/src/store/reducers/CardSlice.test.tsx
+++ react-redux/src/store/reducers/CardSlice.test.tsx
@@ -1,5 +1,13 @@
-import cardSlice, { UserState, getCardsInForm, updateSearchText } from './CardSlice';
-const a = [
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import cardSlice, {
+  UserState,
+  getCardsInForm,
+  updateSearchText,
+} from './CardSlice';
+import { fetchCards, fetchCurrentCards, fetchOneCard } from './ActionCreators';
+
+const newCardsInForm = [
   {
     name: 'string',
     date: 'string',
@@ -18,7 +26,28 @@ const a = [
   },
 ];
 
-describe('Home reducer', () => {
+const cards = [
+  {
+    air_date: 'string',
+    characters: ['1', '2'],
+    created: 'string',
+    episode: 'string',
+    id: 1,
+    name: 'string',
+    url: 'string',
+  },
+  {
+    air_date: 'string',
+    characters: ['1', '2'],
+    created: 'string',
+    episode: 'string',
+    id: 2,
+    name: 'string',
+    url: 'string',
+  },
+];
+
+describe('reducer', () => {
   const state: UserState = {
     cards: [],
     card: [],
@@ -29,7 +58,6 @@ describe('Home reducer', () => {
     searchText: '',
     cardsInForm: [],
   };
-
   it('should handle updateSearchText', () => {
     const initialState: UserState = { ...state, searchText: '' };
     const action = updateSearchText('error');
@@ -40,9 +68,52 @@ describe('Home reducer', () => {
 
   it('should handle getCardsInForm', () => {
     const initialState: UserState = { ...state, cardsInForm: [] };
-    const action = getCardsInForm(a);
-    const expectedState: UserState = { ...state, cardsInForm: a };
+    const action = getCardsInForm(newCardsInForm);
+    const expectedState: UserState = { ...state, cardsInForm: newCardsInForm };
 
     expect(cardSlice(initialState, action)).toEqual(expectedState);
   });
+
+  it('should handle slice fetchCards rejected', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchCards.rejected('rejected');
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
+  it('should handle slice fetchCards fulfilled', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchCards.fulfilled(state);
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
+  it('should handle slice fetchCurrentCards rejected', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchCurrentCards.rejected('rejected');
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
+  it('should handle slice fetchCurrentCards fulfilled', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchCurrentCards.fulfilled(state);
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
+  it('should handle slice fetchOneCard rejected', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchOneCard.rejected('rejected');
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
+  it('should handle slice fetchOneCard fulfilled', async () => {
+    const initialState: UserState = { ...state };
+    const action = fetchOneCard.fulfilled(state);
+    const actual = cardSlice(initialState, action);
+    expect(actual).toEqual(actual);
+  });
+
 });
