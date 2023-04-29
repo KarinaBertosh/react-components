b09250b6bad1f56b0f940713f654a84aef873b9b
KarinaBertosh <cherrycake246@gmail.com>
2023-04-29 13:48:44 +0300

    feat: add vite project

--- react-redux/src/components/MainPage/MainPage.tsx
+++ react-redux/src/components/MainPage/MainPage.tsx
@@ -1,57 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Cards } from '../Cards/Cards';
-import { Header } from '../Header/Header';
-import { Modal } from '../Modal/Modal';
-import { useAppDispatch, useAppSelector } from '../../hook/redux';
-import { cardSlice } from '../../store/reducers/CardSlice';
+import React, { useEffect, useState } from "react";
+import { Cards } from "../Cards/Cards";
+import { Header } from "../Header/Header";
+import { Modal } from "../Modal/Modal";
+import { useAppDispatch, useAppSelector } from "../../hook/redux";
+// import { cardSlice } from '../../store/reducers/CardSlice';
 import {
   fetchCards,
   fetchCurrentCards,
   fetchOneCard,
-} from '../../store/reducers/ActionCreators';
-import './style.scss';
+} from "../../store/reducers/ActionCreators";
+import "./style.scss";
+const cardSlice = require("../../store/reducers/CardSlice.tsx");
 
- export const MainPage = (): JSX.Element => {
-  // const { searchText } = useAppSelector((state) => state.userReducer);
-  // const { cards, card, isLoading, error, errorInCard } =
-  //   useAppSelector((state) => state.userReducer);
-  // const { updateSearchText } = cardSlice.actions;
-  // const dispatch = useAppDispatch();
+export const MainPage = (): JSX.Element => {
+  const { searchText } = useAppSelector((state) => state.userReducer);
+  const { cards, card, isLoading, error, errorInCard } = useAppSelector(
+    (state) => state.userReducer
+  );
+  const { updateSearchText } = cardSlice.actions;
+  const dispatch = useAppDispatch();
 
-  // const [id, setId] = useState(0);
-  // const [modal, setModal] = useState(false);
+  const [id, setId] = useState(0);
+  const [modal, setModal] = useState(false);
 
-  // const sendId = (idCurrent: number) => {
-  //   setId(idCurrent);
-  //   setModal(true);
-  //   dispatch(fetchOneCard(idCurrent));
-  // };
+  const sendId = (idCurrent: number) => {
+    setId(idCurrent);
+    setModal(true);
+    dispatch(fetchOneCard(idCurrent));
+  };
 
-  // const setActive = () => {
-  //   setId(0);
-  //   console.log(id);
-  //   setModal(!modal);
-  // };
+  const setActive = () => {
+    setId(0);
+    console.log(id);
+    setModal(!modal);
+  };
 
-  // const saveChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   dispatch(updateSearchText(e.target.value));
-  // };
+  const saveChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateSearchText(e.target.value));
+  };
 
-  // useEffect(() => {
-  //   dispatch(fetchCards());
-  //   dispatch(fetchCurrentCards(searchText));
-  // }, []);
+  useEffect(() => {
+    dispatch(fetchCards());
+    dispatch(fetchCurrentCards(searchText));
+  }, []);
 
-  // const handleKeyDown = (e: object) => {
-  //   if (e.key === 'Enter') {
-  //     dispatch(fetchCurrentCards(searchText));
-  //   }
-  // };
+  const handleKeyDown = (e: object) => {
+    if (e.key === "Enter") {
+      dispatch(fetchCurrentCards(searchText));
+    }
+  };
 
   return (
     <div className="background">
       Main Page
-      {/* <Header />
+      <Header />
       <div className="search">
         <input
           role="search"
@@ -63,7 +65,7 @@ import './style.scss';
         />
       </div>
       {isLoading && <h1>Loading ...</h1>}
-      {error === '' ? (
+      {error === "" ? (
         <>
           <Cards cards={cards} sendId={sendId} />
           <Modal
@@ -75,7 +77,7 @@ import './style.scss';
         </>
       ) : (
         <h1>{error}</h1>
-      )} */}
+      )}
     </div>
   );
 };
--- react-redux/src/store/store.tsx
+++ react-redux/src/store/store.tsx
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-// import cardSlice from './reducers/CardSlice';
+import cardSlice from './reducers/CardSlice';
 
 const rootReducer = combineReducers({
   cardSlice,
