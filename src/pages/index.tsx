9ee17b148e4946d2e8da2d91b0d55eaf54cb6471
KarinaBertosh <cherrycake246@gmail.com>
2023-10-30 14:40:25 +0300

    feat: add next js basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Main() {
+  return (
+    <>
+      <Head>
+        <title>SSR</title>
+      </Head>
+      <div className="main-page">SSR</div>
+    </>
+  );
+}
