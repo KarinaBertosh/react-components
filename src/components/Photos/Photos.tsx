038f493117989bb6249fb55efdd799e7f8ee014a
KarinaBertosh <cherrycake246@gmail.com>
2023-11-13 18:18:03 +0300

    fix: card's width

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -21,7 +21,7 @@ export const Photos = (props: IPhotosProps): JSX.Element => {
             ? <Image className="plus" src={plus} alt='plus' onClick={() => clickHandler(photo.id)} width={50} />
             : <div></div>
           }
-          <Image src={photo.urls.regular} alt={photo.urls.regular} className="photo" width={50} height={50} priority={true} />
+          <Image src={photo.urls.regular} alt={photo.urls.regular} className="photo" width={1000} height={1000} priority={true} />
         </div>
       ))}
     </div>);
