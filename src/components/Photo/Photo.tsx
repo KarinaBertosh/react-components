8ec41cdb17b8fe34eb13b9de080f1d66c1924fe0
KarinaBertosh <cherrycake246@gmail.com>
2023-11-17 19:17:41 +0300

    fix: buttons in favorite

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -14,12 +14,11 @@ interface IPhotoField {
 
 export default function Photo(props: IPhotoField) {
   const { url, email, setOpenModal } = props;
-  
   return (
     <div className="photo-page">
       <div className="photo-page__navbar">
-        <Image className="icon" src={download} alt='download' width={60} onClick={() => downloadPhoto(url)} />
-        <Image className="icon" src={heart} alt='heart' width={50} onClick={() => sendPhotoToFavorite(url, email)} />
+        <button className="" onClick={() => downloadPhoto(url)}>Dowland photo</button>
+        <button className="" onClick={() => sendPhotoToFavorite(url, email)}>Add photo to favorite</button>
       </div>
       <div className="photo-page__photo-field m-t-20">
         <Image src={backArrow} alt="backArrow" width={35} height={35} onClick={setOpenModal} className="m-r-10 backArrow"></Image>
